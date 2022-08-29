84d161da8921ffedc9352f3233875d26957662ab
RetamrCZ <59335201+RetamrCZ@users.noreply.github.com>
2022-08-29 17:13:24 +0200

    Changed scope for API to work.

--- src/main/java/retamrovec/finesoftware/lifesteal/Listeners/PlayerItemConsumeListener.java
+++ src/main/java/retamrovec/finesoftware/lifesteal/Listeners/PlayerItemConsumeListener.java
@@ -51,7 +51,6 @@ public class PlayerItemConsumeListener implements Listener {
             PlayerEatHeartEvent playerEatHeartEvent = new PlayerEatHeartEvent(e.getPlayer(), e.getItem());
             if (playerEatHeartEvent.isCancelled()) {
                 debug.init("Cancelling PlayerEatHeartEvent because plugin requested it!");
-                return;
             }
             debug.init("Calling playerEatHeartEvent");
             Bukkit.getPluginManager().callEvent(playerEatHeartEvent);
